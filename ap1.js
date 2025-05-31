ece1e0d40e8196ffcf93a812c8ba6d7c6a8badc4
fafalemos <f.lemos@outlook.com.br>
2025-05-30 22:43:48 -0300

    Estruturando...

--- ap1.js
+++ ap1.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
+const routes = path.join(__dirname, 'routes');
+
 
 app.get('/', (req, res) => {
   
-  res.send('Ola');
+  res.sendFile(path.join(routes, 'index.html'));
 });
 
 
